da873f677df9855f906dac057d5605d4f783c5ba
Marvin van Aalst <marvin.vanaalst@gmail.com>
2025-07-16 17:11:18 +0200

    dev: fix: 404 page

--- svelte.config.js
+++ svelte.config.js
@@ -30,7 +30,7 @@ const config = {
 			assets: 'build',
 			precompress: false,
 			strict: true,
-			fallback: '404.html'
+			// fallback: '404.html'
 		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
