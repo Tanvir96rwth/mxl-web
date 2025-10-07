742ade39652d7c692a24cbca48ba728508c65ef2
Tanvir Hassan Rifat <tanvir.aeromist@gmail.com>
2025-10-07 12:41:12 +0200

    Create Math.svelte

---
+++ src/lib/Math.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import katex from "katex";
+  import "katex/dist/katex.min.css";
+  import { onMount } from "svelte";
+
+  export let tex: string;
+  export let display = false;
+
+  let el: HTMLElement;
+  function render() {
+    if (!el) return;
+    katex.render(tex, el, {
+      displayMode: display,
+      throwOnError: false,
+      trust: true,
+      output: "htmlAndMathml"
+    });
+  }
+  onMount(render);
+  $: render();
+</script>
+
+<span bind:this={el} />
